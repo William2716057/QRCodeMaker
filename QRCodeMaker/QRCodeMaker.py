3b26d69fdaa35d09675a0f2ef3b370d2caf5fe06
William Spriggs <Willspri@gmail.com>
2023-10-30 09:40:41 +1300

    updated

--- QRCodeMaker/QRCodeMaker.py
+++ QRCodeMaker/QRCodeMaker.py
@@ -3,16 +3,17 @@ import png
 from pyqrcode import QRCode
 from PIL import Image
 
-
-string = "This is a string"
+#qr code to enter
+string = "example.com"
 
 #generate QR code
 QRcode = pyqrcode.create(string)
 
+#save the QR code as a png image into current directory
 QRcode.png('qr_image.png', scale=6)
 
+#access saved image then display
 QRImage = Image.open('qr_image.png')
 QRImage.show()
-#print(QRcode)
 
 
