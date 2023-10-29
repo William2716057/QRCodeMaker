ceaf35cb0b18e63a9d6cfbc8ff6414aac057f0f9
William Spriggs <Willspri@gmail.com>
2023-10-30 09:38:41 +1300

    updated

--- QRCodeMaker/QRCodeMaker.py
+++ QRCodeMaker/QRCodeMaker.py
@@ -1,6 +1,7 @@
 import pyqrcode
 import png
 from pyqrcode import QRCode
+from PIL import Image
 
 
 string = "This is a string"
@@ -8,9 +9,10 @@ string = "This is a string"
 #generate QR code
 QRcode = pyqrcode.create(string)
 
-QRcode.png('qr_image', scale=6)
+QRcode.png('qr_image.png', scale=6)
 
-
+QRImage = Image.open('qr_image.png')
+QRImage.show()
 #print(QRcode)
 
 
